91e656065711880595e38cecbd19b8cedaa67d75
Izak Burger <isburger@gmail.com>
2014-01-08 11:46:32 +0200

    Ping size option removed. It breaks things.

--- ib/haip/ping.py
+++ ib/haip/ping.py
@@ -3,7 +3,7 @@ from ib.haip.util import sh, CommandFailed
 PING = '/bin/ping' # Make this configurable
 
 def ping(dest_addr, src_addr=None, timeout=2):
-    cmd = [PING, '-s', '1024', '-c', '1', '-w', str(timeout)]
+    cmd = [PING, '-c', '1', '-w', str(timeout)]
     if src_addr is not None:
         cmd.extend(['-I', src_addr])
     cmd.append(dest_addr)
